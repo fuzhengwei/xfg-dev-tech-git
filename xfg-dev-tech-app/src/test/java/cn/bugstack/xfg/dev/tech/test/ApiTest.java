e7ac6da0bbeebc68b1c3e454df65d64bb86a659d
fuzhengwei <184172133@qq.com>
2024-11-10 09:05:03 +0800

    feat: 添加测试代码

--- xfg-dev-tech-app/src/test/java/cn/bugstack/xfg/dev/tech/test/ApiTest.java
+++ xfg-dev-tech-app/src/test/java/cn/bugstack/xfg/dev/tech/test/ApiTest.java
@@ -5,6 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ApiTest {
 
+    public static void  main(String[] args) {
+        Integer x=111 ;
+        System.out.println( x) ;
+    }
+
 }
 
 
