e5d1084f1cadcea7815622cd2315daa6bdff5772
fuzhengwei <184172133@qq.com>
2024-11-10 10:58:58 +0800

    feat:【需求名称】本次代码功能 #pr/#需求ID

--- xfg-dev-tech-app/src/test/java/cn/bugstack/xfg/dev/tech/test/ApiTest.java
+++ xfg-dev-tech-app/src/test/java/cn/bugstack/xfg/dev/tech/test/ApiTest.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ApiTest {
 
+    public Integer xfg = 10;
+
     public static void  main(String[] args) {
         Integer x=111 ;
         System.out.println( x) ;
