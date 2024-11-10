da0c5dbe01243033242f40f1c56892874eecbba0
fuzhengwei <184172133@qq.com>
2024-11-10 09:10:19 +0800

    feat: 添加测试代码

--- xfg-dev-tech-app/src/test/java/cn/bugstack/xfg/dev/tech/test/ApiTest.java
+++ xfg-dev-tech-app/src/test/java/cn/bugstack/xfg/dev/tech/test/ApiTest.java
@@ -2,12 +2,19 @@ package cn.bugstack.xfg.dev.tech.test;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+
 @Slf4j
 public class ApiTest {
 
     public static void  main(String[] args) {
         Integer x=111 ;
         System.out.println( x) ;
+    }
+
+    public void vo2dto(){
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        objectObjectHashMap.put("1","1");
     }
 
 }
